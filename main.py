a14790f568feb4b3a440f37e6d7c0e942d29e8d7
Hendrik Reiter <hendrik.reiter@yahoo.com>
2024-08-15 17:05:21 +0200

    Get new entry point for DEF

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-from provider.provider_registry import ProviderRegistry
-from simulation.simulation import Simulation
-import yaml
+from simulation.distributed_event_factory import DistributedEventFactory
 
 if __name__ == '__main__':
     config_file = "config/example_use_case_assembly_line.yml" #os.environ["CONFIG_FILE"]
-
-    with open(config_file) as file:
-        configuration = yaml.safe_load(file)
-    registry = ProviderRegistry(configuration)
-
-    simulation = Simulation(
-        number_of_data_sources_provider=registry.get("numberOfDataSources"),
-        case_id_provider=registry.get("caseId"),
-        event_loop=registry.get("eventLoop"),
-        data_source_topology_provider=registry.get("dataSourceTopology"),
-    )
-    simulation.start()
+    DistributedEventFactory(config_file).start()
