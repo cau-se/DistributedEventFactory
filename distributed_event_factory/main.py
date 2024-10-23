39c66585a0c90e5d06b7aff405258f4885f4bb47
Hendrik Reiter <hendrik.reiter@yahoo.com>
2024-10-23 09:53:50 +0200

    refactor the test config files

--- distributed_event_factory/main.py
+++ distributed_event_factory/main.py
@@ -1,9 +1,12 @@
+from time import sleep
+
 from distributed_event_factory.event_factory import EventFactory
 
 if __name__ == '__main__':
     event_factory = EventFactory()
     (event_factory
-     .add_directory("../config/assemblyline/simulation")
+     .add_file("../config/assemblyline/simulation/simulation_countbased.yaml")
      .add_directory("../config/assemblyline/datasource")
-     .add_directory("../config/assemblyline/sink/loadtest")
+     .add_file("../config/assemblyline/sink/kafka-sink.yaml")
      ).run()
+    sleep(1)
