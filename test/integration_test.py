cdb48b0e782ff3318fca3dbeb30d46064d4f652f
Hendrik Reiter <hendrik.reiter@yahoo.com>
2024-11-08 16:30:37 +0100

    change folder of integration test

--- test/integration_test.py
+++ test/integration_test.py
@@ -10,9 +10,9 @@ class IntegrationTest(unittest.TestCase):
         event_factory = EventFactory()
         event_factory.add_sink_parser("test", TestSinkParser())
         (event_factory
-         .add_directory(directory="config/assemblyline/datasource")
+         .add_directory(directory="config/datasource/assemblyline")
          .add_file("config/simulation/countbased.yaml")
-         .add_file("config/sink/loadtest.yaml")
+         .add_file("config/sink/test-sink.yaml")
          .run())
         sink_mock: TestSink = event_factory.get_sink("test")
         self.assertEqual(100, len(sink_mock.event_log))
