61047354c4ca2cf5da10876157ff2f934cc64cff
ulpi <ulrike.pieper@ppi.de>
2025-05-28 10:33:25 +0200

    add parsing of input objects and start of output

---
+++ distributed_event_factory/core/core_decisions_datasource.py
@@ -0,0 +1,11 @@
+class GenericDecisions:
+    def __init__(
+            self,
+            simulation_provider,
+            type_parser
+    ):
+        self.simulation_provider = simulation_provider
+        self.type_parser= type_parser
+
+    def get_event_data(self):
+        return self.simulation_provider.get_event_data()
