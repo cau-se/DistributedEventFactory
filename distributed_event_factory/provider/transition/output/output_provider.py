c8e573abc2807520cf7a57e34b82f4d4791627f1
ulpi <ulrike.pieper@ppi.de>
2025-06-06 14:35:28 +0200

    add object states

--- distributed_event_factory/provider/transition/output/output_provider.py
+++ distributed_event_factory/provider/transition/output/output_provider.py
@@ -1,7 +1,11 @@
 class OutputObjectProvider:
-    def __init__(self, objectName, numberOfObject):
+    def __init__(self, objectName, numberOfObject, change):
         self.objectName = objectName
         self.numberOfObject = numberOfObject
+        self.change = change
 
     def __str__(self):
-        return str(self.__dict__)
+        if self.change:
+            return str(self.__dict__)
+        else:
+            return str({key: value for key, value in self.__dict__.items() if value})
