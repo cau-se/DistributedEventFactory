7b036d2ec69d4eac3d436132920941c68d1d81aa
ulpi <ulrike.pieper@ppi.de>
2025-06-27 14:39:42 +0200

    add workstation layer and first attempt to fix the time

--- distributed_event_factory/simulation/process_simulation_object_centric.py
+++ distributed_event_factory/simulation/process_simulation_object_centric.py
@@ -10,6 +10,7 @@ from provider.object.input.input_provider import InputObjectProvider
 from simulation.simulator_objects.object_storage import ObjectStorage
 from simulation.simulator_objects.object_utility import ObjectUtility
 from simulation.simulator_objects.workprocessstep import WorkProcessStep
+from simulation.simulator_objects.workstation import WorkStation
 from simulation.simulator_objects.workstation_service import WorkstationService
 
 
@@ -21,7 +22,7 @@ class ProcessSimulationObjectCentric:
             objects: Dict[str, ObjectData],
             stocks: Dict[str, InputObjectProvider]
     ):
-        self.workstation_steps = []
+        self.workstations = []
         self.workstation_service = WorkstationService()
         self.current_timestamp = datetime.now()
         self.object_storage = object_storage
@@ -33,13 +34,14 @@ class ProcessSimulationObjectCentric:
         self.prior_step : str = ""
 
     def simulate(self) -> Event:
-        available_steps: List[WorkProcessStep] = (
-            self.workstation_service.get_activatable_workstations(self.workstation_steps, self.object_storage))
-        next_step = self.get_next_step(available_steps)
+        available_workstations: List[WorkStation] = (
+            self.workstation_service.get_activatable_workstations(self.workstations, self.object_storage))
+        next_step, next_workstation = self.get_next_workstation(available_workstations)
         self.prior_step = next_step.node
-        event = next_step.produce_event(self.current_timestamp)
+        event = next_step.produce_event(self.current_timestamp, next_workstation.work_station_name)
         self.object_storage.manage_input_and_output_of_steps(next_step.input_objects, next_step.output_objects,
                                                              self.objects, self.current_timestamp)
+        self.current_timestamp = next_step.last_event_end_timestamp
         return event
 
     def _get_next_step_by_event_provider(self, data_source) -> List[str]:
@@ -53,17 +55,20 @@ class ProcessSimulationObjectCentric:
             return next_sensors
         return None
 
-    def get_next_step(self, available_steps):
+    def get_next_workstation(self, available_workstations):
         if self.prior_step:
             next_available_steps = self._get_next_step_by_event_provider(self.prior_step)
             if next_available_steps:
-                next_step: WorkProcessStep = self.workstation_service.get_workstation_preselected(available_steps,
-                                                                                                  next_available_steps)
+                next_workstation: WorkStation = self.workstation_service.get_workstation_preselected(available_workstations,
+                                                                                                  next_available_steps, self.object_storage)
+                next_step = next_workstation.get_workstation_preselected(object_storage=self.object_storage, prefered_workstation_steps=next_available_steps)
             else:
-                next_step: WorkProcessStep = self.workstation_service.get_random_workstation(available_steps)
+                next_workstation: WorkStation = self.workstation_service.get_random_workstation(available_workstations)
+                next_step = next_workstation.get_random_workstation_step()
         else:
-            next_step: WorkProcessStep = self.workstation_service.get_random_workstation(available_steps)
-        return next_step
+            next_workstation: WorkStation = self.workstation_service.get_random_workstation(available_workstations)
+            next_step = next_workstation.get_random_workstation_step()
+        return next_step, next_workstation
 
     def _get_sensor_with_id(self, data_source_id) -> DataSource:
         for sensor in self.data_sources:
@@ -84,12 +89,16 @@ class ProcessSimulationObjectCentric:
                 data_source_id = DataSourceId(data_source)
                 events = self._get_sensor_with_id(DataSourceId(data_source)).get_event_data()
                 input_objects = self._get_sensor_with_id(DataSourceId(data_source)).event_provider.input_objects
+                workstation = self._get_sensor_with_id(DataSourceId(data_source)).event_provider.workstation
+                workstation_steps = []
                 for e in events:
-                    self.workstation_steps.append(WorkProcessStep(activity=e.get_activity_provider().get_activity(),
+                    workstation_steps.append(WorkProcessStep(activity=e.get_activity_provider().get_activity(),
                                                                   node=data_source_id.get_name(),
                                                                   group_id=self._get_sensor_with_id(
                                                                       data_source_id).group_id,
                                                                   input_objects=input_objects,
                                                                   output_objects=e.get_activity_provider().get_output(),
                                                                   duration=e.get_duration()))
+
+                self.workstations.append(WorkStation(work_station_name=workstation ,work_process_steps=workstation_steps))
 
